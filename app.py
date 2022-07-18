ddc423da83c44bb845eaa40d8b705ed3339e8286
bulabd <bulabd@outlook.com>
2022-07-18 15:40:18 -0400

    Learn variables

---
+++ app.py
@@ -0,0 +1,14 @@
+####### BASICS #######
+# print("Hello World!")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+
+####### VARIABLES #######
+
+a_variable = "Bulat"
+age = 20
+is_male = True
+print("Hello " + a_variable + ", you are " + str(age) + " years old")
