fcfeae755a2dd40273c31c437c0d73ce6d7f3d40
bulabd <bulabd@outlook.com>
2022-07-19 13:32:20 -0400

    Strings and methods

--- app.py
+++ app.py
@@ -7,8 +7,20 @@
 
 
 ####### VARIABLES #######
+# a_variable = "Bulat"
+# age = 20
+# is_male = True
+# print("Hello " + a_variable + ", you are " + str(age) + " years old")
 
-a_variable = "Bulat"
-age = 20
-is_male = True
-print("Hello " + a_variable + ", you are " + str(age) + " years old")
+
+####### WORKING WITH STRINGS #######
+# print('Giraffe "Academy"')
+# print("Giraffe\"Academy")
+# print("Giraffe\\Academy")
+# phrase = 'Giraffe Academy'
+# print(len(phrase.upper()))
+# print(phrase[8])
+# print(phrase.index('z'))
+# print(phrase.replace("Giraffe", "Lion"))
+
+
