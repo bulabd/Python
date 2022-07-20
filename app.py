3c1691f3d5b3a29f2a6e4b6af24dce848847a994
bulabd <bulabd@outlook.com>
2022-07-20 14:38:35 -0400

    Learn lists, numbers, input

--- app.py
+++ app.py
@@ -23,4 +23,44 @@
 # print(phrase.index('z'))
 # print(phrase.replace("Giraffe", "Lion"))
 
+####### WORKING WITH NUMBERS #######
+# print(3 + 5.09)
+# print(3 * (4 + 5))
+# print(12 % 5)
+# my_num = -7
+# print(str(my_num) + " is a number")
+# print(abs(my_num))
+# print(pow(my_num, 2))
+# print(max(5, 20))
+# print(min(20, 1))
+# print(round(3.72232))
+from math import *
+# print(floor(4.99))
+# print(ceil(3.1))
+# print(sqrt(36))
 
+####### GET INPUT FROM USER #######
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "! You are " + age + " years old!")
+
+####### BASIC CALCULATOR #######
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+
+####### MAD LIBS GAME #######
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a plural noun: ")
+# celebrity = input("Enter a celebrity: ")
+# print("Roses are {color}".format(color=color))
+# print("{plural_noun} are blue".format(plural_noun = plural_noun))
+# print("I love {celebrity}".format(celebrity = celebrity))
+
+####### LISTS (Arrays) #######
+# list = ["Bob", "Man", "Bruh", "OK", 20, 22, 1, 54, True, False]
+# list[1] = "Bulat"
+# print(list[-3])
+# print(list[1:3])
+# print(list[1])
